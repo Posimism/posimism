929b91425449d7b1ee9ab1ef997deb2db4c423d5
OuttaPencil <tweyer@q.com>
2025-03-03 21:52:17 -0700

    Secret api key

--- posimism/amplify/functions/save-and-generate-message/resource.ts
+++ posimism/amplify/functions/save-and-generate-message/resource.ts
@@ -1,6 +1,8 @@
-import { defineFunction } from "@aws-amplify/backend";
+import { defineFunction, secret, } from "@aws-amplify/backend";
 
 export const saveAndGenerateMessage = defineFunction({
   timeoutSeconds: 180,
-  environment: {},
+  environment: {
+    OPENAI_API_KEY: secret("OPENAI_API_KEY"),
+  },
 });
