d32afeec3f455ae1e09b9886a79305ec22556cfc
OuttaPencil <tweyer@q.com>
2025-03-05 10:15:12 -0700

    Actually useful eslint for amplify/**/*.js

--- posimism/eslint.config.mjs
+++ posimism/eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,6 +11,19 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  js.configs.recommended,
+  {
+    ignores: ["node_modules/**", "dist/**"],
+  },
+  {
+    files: ["**/*.js"],
+    rules: {
+      "no-undef": "error",
+      "no-unused-vars": "error",
+      "no-self-compare": "error",
+      "no-use-before-define": "error",
+    },
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
